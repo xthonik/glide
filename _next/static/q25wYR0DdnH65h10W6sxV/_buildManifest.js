aafb6f60dd9c77d4deffcf26e1b6255aed1e666b
xthonik <64434276+xthonik@users.noreply.github.com>
2026-03-25 20:40:36 +0500

    Deploy GitHub Pages

---
+++ _next/static/q25wYR0DdnH65h10W6sxV/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/easing-tool//_next/:path+",
+        "destination": "/easing-tool/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
